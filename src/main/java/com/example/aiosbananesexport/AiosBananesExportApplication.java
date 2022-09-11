2d39d9a7f03cfa0f93504f66e974f9a45f30ca55
mbimbij <joseph.mbimbi@gmail.com>
2022-09-11 10:10:22 +0200

    Initial Commit

---
+++ src/main/java/com/example/aiosbananesexport/AiosBananesExportApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiosbananesexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiosBananesExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiosBananesExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aiosbananesexport/recipient/RecipientShould.java
@@ -0,0 +1,12 @@
+package com.example.aiosbananesexport.recipient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RecipientShould {
+    @Test
+    void name() {
+        assertThat(true).isTrue();
+    }
+}
