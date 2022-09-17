f7c40dcaebfc0363a5906b81dcbfad67eb787464
mbimbij <joseph.mbimbi@gmail.com>
2022-09-17 10:00:25 +0200

    Introduce POST body and reponse

--- src/main/java/com/example/aiosbananesexport/infra/in/ApplicationRestController.java
+++ src/main/java/com/example/aiosbananesexport/infra/in/ApplicationRestController.java
@@ -2,6 +2,7 @@ package com.example.aiosbananesexport.infra.in;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,7 +12,7 @@ public class ApplicationRestController {
 
     @PostMapping(value = "/order")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Mono<CreateOrderResponseDto> createOrder() {
+    public Mono<CreateOrderResponseDto> createOrder(@RequestBody CreateOrderRequestDto requestDto) {
         CreateOrderResponseDto responseDto = new CreateOrderResponseDto("id",
                                                                         "firstName",
                                                                         "lastName",
@@ -19,7 +20,7 @@ public class ApplicationRestController {
                                                                         75019,
                                                                         "paris",
                                                                         "france",
-                                                                        "somedate",
+                                                                        requestDto.getDeliveryDate(),
                                                                         50);
         return Mono.just(responseDto);
     }
--- src/test/java/com/example/aiosbananesexport/ApplicationShould.java
+++ src/test/java/com/example/aiosbananesexport/ApplicationShould.java
@@ -1,13 +1,18 @@
 package com.example.aiosbananesexport;
 
 import com.example.aiosbananesexport.infra.in.CreateOrderRequestDto;
+import com.example.aiosbananesexport.infra.in.CreateOrderResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationShould {
@@ -15,17 +20,54 @@ class ApplicationShould {
     @Autowired
     private WebTestClient webTestClient;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    void contextLoads() {
+    void load_context_with_no_problem() {
     }
 
     @Test
     void create_an_order__in_nominal_case() {
-        new CreateOrderRequestDto("firstName", "lastName", "address", 75019, "paris", "france", LocalDate.now().plusWeeks(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")), 50);
-        webTestClient.post()
-                .uri("/order")
-                .exchange()
+        // GIVEN
+        String firstName = "firstName";
+        String lastName = "lastName";
+        String address = "address";
+        int postalCode = 75019;
+        String city = "paris";
+        String country = "france";
+        String deliveryDate = LocalDate.now()
+                                       .plusWeeks(1)
+                                       .format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        int quantityKg = 50;
+        CreateOrderRequestDto requestDto = new CreateOrderRequestDto(firstName, lastName, address, postalCode, city, country, deliveryDate, quantityKg);
+        CreateOrderResponseDto expectedResponseDto = new CreateOrderResponseDto("any", firstName, lastName, address, postalCode, city, country, deliveryDate, quantityKg);
+
+        // WHEN
+        WebTestClient.ResponseSpec responseSpec = webTestClient.post()
+                                                               .uri("/order")
+                                                               .bodyValue(requestDto)
+                                                               .exchange();
+        // THEN the REST response is correct
+        responseSpec
                 .expectStatus()
-                .isCreated();
+                .isCreated()
+                .expectBody()
+                .consumeWith(result -> {
+                    try {
+                        byte[] responseBody = result.getResponseBody();
+                        CreateOrderResponseDto actualResponseDto = objectMapper.readValue(responseBody, CreateOrderResponseDto.class);
+                        assertSoftly(softAssertions -> {
+                            softAssertions.assertThat(actualResponseDto)
+                                          .usingRecursiveComparison()
+                                          .ignoringFields("id")
+                                          .isEqualTo(expectedResponseDto);
+                            softAssertions.assertThat(actualResponseDto.getId())
+                                          .isNotBlank();
+                        });
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
